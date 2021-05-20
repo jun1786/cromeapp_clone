e6d4cd009c6236c0ce6c00e1170e1a42fd27bc7f
jun1786 <jun1786@users.noreply.github.com>
2021-05-20 16:02:47 +0900

    1
    first upload

---
+++ js/clock.js
@@ -0,0 +1,21 @@
+ const clockContainer = document.querySelector(".js-clock");
+  clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+      years = date.get
+      milliseconds = date.getMilliseconds();
+      seconds = date.getSeconds();
+      minutes = date.getMinutes();
+      hours = date.getHours();
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+  function init(){
+    getTime();
+    setInterval(getTime, 1);
+}
+init();
